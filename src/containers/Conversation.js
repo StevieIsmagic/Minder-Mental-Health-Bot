e1b0e2da795a3e270c594813ebd96e7e8c31e6c8
Colombianito66 <colombianito66@gmail.com>
2017-11-03 19:57:30 -0400

    progress on Conversation component

--- src/containers/Conversation.js
+++ src/containers/Conversation.js
@@ -6,3 +6,81 @@ import React, { Component } from 'react';
 import autoBind from 'auto-bind';
 import { ThemeProvider } from 'styled-components';
 import theme from '../theme';
+
+import Container from '../primitives/Container';
+import UserInput from '../primitives/UserInput';
+import Message from '../components/Message';
+import MessageArea from '../primitives/MessageArea';
+import Loading from '../components/Loading';
+import SubmitButton from '../primitives/SubmitButton';
+
+class Conversation extends Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+    this.state = {
+      questions: props.questions.map(question => {
+        return {
+          ...question,
+          sender: 'BOT',
+        };
+      }),
+      questionNumber: 0,
+      userInput: '',
+      disabledUserInput: false,
+      messages: [],
+      answers: {},
+      loadingBot: false,
+    }
+  }
+
+  componentWillMount() {
+    const { questions, questionNumber } = this.state;
+
+    this.setState({
+      ...this.state,
+      messages: [
+        questions[questionNumber],
+      ]
+    })
+  }
+
+  componentDidMount() {
+    this.userInput.focus();
+  }
+
+  handleUserInput(e) {
+    e.preventDefault();
+    this.setState({
+      userInput: e.target.value,
+    })
+  }
+
+  handleButtonSelect(select) {
+    this.setState({
+      messages: [
+        ...this.state.messages,
+        {
+          text: select.text,
+          type: 'USER'
+        }
+      ],
+      answers: this.state.questions[this.state.questionNumber].key ? 
+      { ...this.state.answers,
+        [this.state.questions[this.state.questionNumber].key]: select.value,
+      } : { ...this.state.answers, },
+    }, () => {
+      this.nextQuestion();
+    })
+  }
+  
+  finalMessage() {
+    return {
+      text: 'Thanks, talk to you soon!',
+      type: 'final',
+      sender: 'BOT',
+    }
+  }
+
+
+}
