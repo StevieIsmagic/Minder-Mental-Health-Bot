b3b254bfa283d2245f40721803045a504f93ed7b
Colombianito66 <colombianito66@gmail.com>
2017-11-03 22:24:49 -0400

    default theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+/*
+* DEFAULT THEME
+*/
+
+const theme = {
+  blobColor: '#eee',        // grey white ?
+  userBlobColor: '#0066ff', // royal blue
+  user: '🙂',
+  bot: '🤖',
+  baseColor: '#eee',        // grey white ?
+  font: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif',
+  height: '500px',
+  width: '500px',
+}
