bf6c6e86833afe96000be00bf4a16d173f723e4f
Colombianito66 <colombianito66@gmail.com>
2017-11-05 21:48:31 -0500

    complete message component

---
+++ src/components/Message.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { withTheme } from 'styled-components';
+
+import { MessageBlobBot, MessageBlobUser } from '../primitives/MessageBlob';
+import Avatar from '../primitives/Avatar';
+import AnswerButton from '..primitives/AnswerButton';
+import Image from '../primitives/Image';
+
+const Message = (props) => {
+  const { message, active, theme } = props;
+
+  return (
+    <section>
+      {message.sender === 'BOT' ? 
+        <div>
+          <Avatar left>
+            {theme.bot}
+          </Avatar>
+          <MessageBlobBot>
+            {message.text}
+          </MessageBlobBot>
+          {message.image &&
+            <Image>
+              <img src={message.image} alt={message.text} />
+            </Image>
+          }
+          {message.buttons && active &&
+            <div>
+              {message.buttons.map((button, index) => 
+                <AnswerButton key={index} onClick={() => props.onButtonSelect(button)}>
+                  {button.text}
+                </AnswerButton>
+              )}
+            </div>
+          }
+          {/*message.type === 'final' &&
+            Object.keys(answers).map((answer, index) =>
+              <div key={index}>
+                {answer}
+                <span>{answers[answer]}</span>
+              </div>
+            )
+          */}
+        </div>
+        :
+        <div>
+          <Avatar right>
+            {theme.user}
+          </Avatar>
+          <MessageBlobUser>
+            {message.text}
+          </MessageBlobUser>
+        </div>
+      }
+    </section>
+  );
+}
+
+export default withTheme(Message);
